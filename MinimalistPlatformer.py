df344c46d2b31fa90b108b17384569c41ffd67e8
chrism <>
2009-01-04 15:44:22 +0000

    Minor tweaks for windows building, icon, etc.

--- MinimalistPlatformer.py
+++ MinimalistPlatformer.py
@@ -1,5 +1,8 @@
 from Core import Core
 from PodSix.SplashScreen import SplashScreen
+from PodSix.Resource import *
+
+gfx.SetIcon("resources/icon.gif")
 
 s = SplashScreen()
 s.Launch()
---
+++ windows-build.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+from sys import argv
+from os import path, listdir, mkdir
+import shutil
+import py2exe
+
+# change me
+project_name = "MinimalistPlatformer"
+
+# extra datafiles to be copied from the resources dir
+datafiles = [path.join("resources", d) for d in listdir('resources')]
+
+# seed the install command line with what we want
+argv += ["py2exe"]
+setup(
+	windows = [
+		{
+			"script": project_name + ".py",
+			"icon_resources": [(1, path.join("resources", "main.ico"))]
+		}
+	],
+)
+
+# also need to hand copy the extra files here
+def installfile(name):
+	dst = path.join('dist', 'resources')
+	if not path.isdir(dst):
+		print 'making dir', dst
+		mkdir(dst)
+	print 'copying', name, '->', dst
+	if path.isdir(name):
+		dst = path.join(dst, name)
+		if path.isdir(dst):
+			shutil.rmtree(dst)
+		shutil.copytree(name, dst)
+	elif path.isfile(name):
+		shutil.copy(name, dst)
+	else:
+		print 'Warning, %s not found' % name
+
+[installfile(data) for data in datafiles]
+
