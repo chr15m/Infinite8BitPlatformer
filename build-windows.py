56cd0dcfcce303536cf2a133b6c47e067d205d7c
Chris McCormick <chrism@mccormick.cx>
2010-03-28 12:24:07 +0800

    Windows build script fixed.

--- build-windows.py
+++ build-windows.py
@@ -1,14 +1,27 @@
 from distutils.core import setup
 from sys import argv
-from os import path, listdir, mkdir, chdir
+from os import path, listdir, mkdir, chdir, getcwd, system
+from subprocess import Popen, PIPE
 import shutil
-import py2exe
-from os import getcwd
 
 # delete the old build and dist directories for a clean start
 for d in ["build", "dist"]:
 	if path.isdir(d):
 		shutil.rmtree(d)
+
+# hack to include simplejson egg in the build
+import pkg_resources
+eggs = pkg_resources.require("simplejson")
+
+from setuptools.archive_util import unpack_archive
+for egg in eggs:
+	if path.isdir(egg.location):
+		shutil.copytree(egg.location, ".")
+	else:
+		unpack_archive(egg.location, ".")
+		shutil.rmtree("EGG-INFO")
+
+import py2exe
 
 # get the name of this directory for the project_name
 project_name = path.basename(getcwd())
@@ -22,7 +35,15 @@ setup(
 			"icon_resources": [(1, path.join("resources", "main.ico"))],
 		}
 	],
+	#options = {"py2exe": {"packages": ["simplejson"]}},
 )
 
 # hand copy the resources directory
 shutil.copytree("resources", path.join("dist", "resources"))
+
+revno = Popen(["bzr", "revno"], stdout=PIPE).communicate()[0].rstrip()
+if path.isdir(project_name):
+	shutil.rmtree(project_name)
+shutil.move("dist", "%s" % project_name)
+system("zip -r %s-%s-windows.zip %s" % (project_name, revno, project_name))
+shutil.rmtree("build")
