0161c684d8ba97db51bd7ff8dd462884f8e847e8
Chris McCormick <chris@mccormick.cx>
2011-03-03 20:40:20 +0800

    Use the utf8 friendly font in speech bubbles for now.

--- engine/SpeechBubble.py
+++ engine/SpeechBubble.py
@@ -10,7 +10,7 @@ class SpeechBubble(TimedText):
 		if not kwargs.has_key('time') or not kwargs['time']:
 			kwargs['time'] = 0.2
 		kwargs['color'] = [75, 75, 75]
-		#kwargs['font'] = 'speech'
+		kwargs['font'] = 'speech'
 		
 		self.black = [0, 0, 0]
 		self.white = [255, 255, 255, 150]
